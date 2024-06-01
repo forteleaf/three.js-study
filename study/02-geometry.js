4949c1ad00a48364ef43c62de653c623a5a4e5a7
forteleaf <leaf@korpay.com>
2024-06-01 23:52:52 +0900

    PlaneGeometry

--- study/02-geometry.js
+++ study/02-geometry.js
@@ -43,7 +43,7 @@ class App {
     new OrbitControls(this._camera, this._divContainer);
   }
   _setupModel() {
-    const geometry = new THREE.RingGeometry(0.2, 1, 5, 2, 0, Math.PI);
+    const geometry = new THREE.PlaneGeometry(1, 1.4, 1);
     const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
     const cube = new THREE.Mesh(geometry, fillMaterial);
 
