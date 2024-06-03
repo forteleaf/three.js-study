7c9e0ac205fba7690bfcf7312967f5ba4e55ed62
forteleaf <leaf@korpay.com>
2024-06-04 08:21:54 +0900

    Scene Graph: 태양,지구,달

--- study/02-geometry.js
+++ study/02-geometry.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { PhongLightingModel } from "three/examples/jsm/nodes/Nodes.js";
 
 console.log("load js");
 class App {
@@ -29,7 +30,7 @@ class App {
     const width = this._divContainer.clientWidth;
     const height = this._divContainer.clientHeight;
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-    camera.position.z = 2;
+    camera.position.z = 25;
     this._camera = camera;
   }
   _setupLights() {
@@ -42,22 +43,69 @@ class App {
   _setupControls() {
     new OrbitControls(this._camera, this._divContainer);
   }
+  // _setupModel() {
+  //   const geometry = new THREE.TorusKnotGeometry(0.6, 0.1, 100, 32, 3, 2);
+  //   const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+  //   const cube = new THREE.Mesh(geometry, fillMaterial);
+
+  //   const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+  //   const line = new THREE.LineSegments(
+  //     new THREE.WireframeGeometry(geometry),
+  //     lineMaterial
+  //   );
+  //   const group = new THREE.Group();
+  //   group.add(cube);
+  //   group.add(line);
+
+  //   this._scene.add(group);
+  //   this._cube = group;
+  // }
   _setupModel() {
-    const geometry = new THREE.TorusKnotGeometry(0.6, 0.1, 100, 32, 3, 2);
-    const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
-    const cube = new THREE.Mesh(geometry, fillMaterial);
+    const solarSystem = new THREE.Object3D();
+    this._scene.add(solarSystem);
 
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
-    const line = new THREE.LineSegments(
-      new THREE.WireframeGeometry(geometry),
-      lineMaterial
+    const radius = 1;
+    const widthSegments = 12;
+    const heightSegments = 12;
+    const sphereGeometry = new THREE.SphereGeometry(
+      radius,
+      widthSegments,
+      heightSegments
     );
-    const group = new THREE.Group();
-    group.add(cube);
-    group.add(line);
+    const sunMaterial = new THREE.MeshPhongMaterial({
+      emissive: 0xffff00,
+      flatShading: true,
+    });
+    const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+    sunMesh.scale.set(3, 3, 3);
+    solarSystem.add(sunMesh);
+    const earthOrbit = new THREE.Object3D();
+    solarSystem.add(earthOrbit);
+    const earthMaterial = new THREE.MeshPhongMaterial({
+      color: 0x2233ff,
+      emissive: 0x112244,
+      flatShading: true,
+    });
+    const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+    earthOrbit.position.x = 10;
+    earthOrbit.add(earthMesh);
 
-    this._scene.add(group);
-    this._cube = group;
+    const moonOrbit = new THREE.Object3D();
+    moonOrbit.position.x = 2;
+    earthOrbit.add(moonOrbit);
+
+    const moonMaterial = new THREE.MeshPhongMaterial({
+      coloe: 0x888888,
+      emissive: 0x222222,
+      flatShading: true,
+    });
+    const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+    moonMesh.scale.set(0.5, 0.5, 0.5);
+    moonOrbit.add(moonMesh);
+
+    this._solarSystem = solarSystem;
+    this._earthOrbit = earthOrbit;
+    this._moonOrbit = moonOrbit;
   }
   resize() {
     const width = this._divContainer.clientWidth;
@@ -77,6 +125,9 @@ class App {
     time *= 0.001;
     // this._cube.rotation.x = time;
     // this._cube.rotation.y = time;
+    this._solarSystem.rotation.y = time / 2;
+    this._earthOrbit.rotation.y = time * 2;
+    this._moonOrbit.rotation.y = time * 5;
   }
 }
 
